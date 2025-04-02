032ee62029f92f3633f70b81b05999717e6b11d0
Manup <314.developer@gmail.com>
2025-04-02 07:04:30 +0530

    Fixing navigation

---
+++ src/ServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Developer314\Vitalis;
+
+use Statamic\Providers\AddonServiceProvider;
+
+class ServiceProvider extends AddonServiceProvider
+{
+    public function bootAddon()
+    {
+        //
+    }
+}
